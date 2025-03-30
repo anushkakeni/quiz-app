13503a8ee8f69ed9b4730a38d87038725e5f8ab3
anushkakeni <kenianushka@gmail.com>
2025-03-30 07:21:26 +0530

    quiz-app  added

--- script.js
+++ script.js
@@ -56,8 +56,6 @@ document.getElementById("signupForm").addEventListener("submit", function (event
 
 
 
-
-
 // login
 
 const login = document.getElementById("abc")
@@ -92,45 +90,113 @@ function loginBtn() {
 
 
 
-// quiz questions local storage
-//  for random questions
-//  use array of objects
-[
-    {
-        id:1,
-        question: "this is the question",
-        options:["asdfop","qw5",3,4],
-        userAnswer:0,
-        correctOption:3
-    },
-    {
-        id:1,
-        question: "this is the question",
-        options:["asdfop","qw5",3,4],
-        userAnswer:0,
-        correctOption:3
-    },
-    {
-        id:1,
-        question: "this is the question",
-        options:["asdfop","qw5",3,4],
-        userAnswer:0,
-        correctOption:3
-    },
-    {
-        id:1,
-        question: "this is the question",
-        options:["asdfop","qw5",3,4],
-        userAnswer:0,
-        correctOption:3
-    },
-    {
-        id:1,
-        question: "this is the question",
-        options:["asdfop","qw5",3,4],
-        userAnswer:0,
-        correctOption:3
-    },
-]
 
-randomIndex, counter1=-10, questions=[2,5,]// index geenrated or question id
+
+
+//quiz-question
+
+// quiz questions local storage
+//  for random questions:- use array of objects
+let quizQuestions= [
+    {
+        id:1,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:2,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:3,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:4,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:5,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:6,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:7,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:8,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:9,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    },
+    {
+        id:10,
+        question:"What is the capital of France?" ,
+        options: ["Paris", "London", "Berlin", "Rome"],
+        userAnswer: "",
+        correctOption:"Paris"
+    }
+];
+
+if (localStorage.getItem('quizQuestion') === null) {
+    localStorage.setItem('quizQuestion', JSON.stringify(questions));
+}
+
+//set counter for question nos.
+let currentQuestionIndex = 1;
+
+function nextQuestion(){
+
+//parse and get data from local storage to array of object
+  
+  let getQuestionsAndAnswers = JSON.parse(localStorage.getItem("quizQuestions"));
+
+//radomly generate the questions in array
+  let randomQuestions = Math.floor(Math.random() * getQuestions.length);
+
+//access data from array of objects
+//array[random].objectKey
+  let getQuestions = getQuestionsAndAnswers[randomQuestions].question;
+  let getCorrect =  getQuestionsAndAnswers[randomQuestions].correctOption;
+
+
+   document.getElementById("question-counter").innerText = "Question" + (currentQuestionIndex + 1) + "of 10";
+   
+   document.getElementById("question-text").innerText = (currentQuestionIndex + 1) + "." + getQuestions;
+
+
+}
+
+randomIndex ok , counter1=-10, questions=[2,5,]// index geenrated or question id
